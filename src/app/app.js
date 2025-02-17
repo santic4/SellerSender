acbd7e65dda1cdd2fc93a822b6a7acab8fc9c715
Santiago Cañu <santisv4@gmail.com>
2025-02-17 17:10:30 -0300

    mm

--- src/app/app.js
+++ src/app/app.js
@@ -67,4 +67,3 @@ app.get('*', (req, res) => {
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
-console.log(CLIENT_ID,'client', CLIENT_SECRET,'secrete')
